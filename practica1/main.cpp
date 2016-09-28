9f194182a7101db6255c9bafed22d4e1c7fe23d2
Reshait <reshait@outlook.com>
2016-09-28 14:45:10 +0200

    OK Clase matriz

--- practica1/main.cpp
+++ practica1/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 #include "datosEntrada.hpp"
 #include "cabecera.hpp"
+#include "matriz.hpp"
 
 //DNI ...1L --> opción del determinante.
 
 int main(){
+    srand(time(NULL));
+
 	DatosEntrada Entrada;
 	cabecera(1);	
 
-	Entrada.pideDatos();
-	Entrada.imprimeDatos();
+//	Entrada.pideDatos();
+//	Entrada.imprimeDatos();
+
+//	Matriz M=new Matriz(3); 
+	Matriz M(3);
+	M.imprimeMatriz();
 }
---
+++ practica1/matriz.hpp
@@ -0,0 +1,65 @@
+/*! 
+  \file     mamtriz.hpp
+  \brief    Función y Estructura correspondiente para rellenar los datos de entrada.
+  \author   Teófilo Rojas Mata
+  \date     28/09/2016
+*/
+
+#ifndef __MATRIZ_HPP__
+#define __MATRIZ_HPP__
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+/*! 
+  \brief Definición clase Matriz creada para los datos de entrada a pedir por el teclado.
+  \post Ninguna
+*/
+
+// Sacado de:
+// http://www.sc.ehu.es/sbweb/fisica/cursoJava/numerico/matrices/matriz/matriz.htm
+class Matriz
+{
+	private:
+		double** _m;
+		int _tam;
+
+	public:
+		Matriz(int tam){
+			setTam(tam);
+
+			_m = new double*[getTam()];
+			for(int i = 0; i < getTam(); i++){
+			
+				_m[i] = new double[getTam()];
+			
+				for(int j = 0; j < getTam(); j++){
+					_m[i][j] = (rand() % 10) - 10;
+				}
+			}
+		}
+
+		inline const int getTam(){ return _tam; }
+		inline void setTam(int tam){ _tam = tam; }
+
+
+		void imprimeMatriz(){
+			int cuentaIntro = 0;
+
+			for(int i = 0; i < getTam(); i++){
+				for(int j = 0; j < getTam(); j++){
+					cout << _m[i][j] << "  ";
+					
+					cuentaIntro ++;					
+					if(cuentaIntro == getTam()){
+						cout << endl;
+						cuentaIntro = 0;
+					}
+				}
+			}
+		}
+	
+};
+
+#endif
